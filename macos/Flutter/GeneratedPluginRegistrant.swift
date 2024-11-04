43d586a5e08802c91f3770dc62f17616e5f114af
JuanJoseUTP0146906 <90533222+JuanJoseUTP0146906@users.noreply.github.com>
2024-11-04 14:01:26 -0600

    cambios 4
    cambios a los archivos build gradle y se anexan mas dependencias

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,8 @@ import Foundation
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
-import path_provider_foundation
-import sqflite_darwin
+import path_provider_macos
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
