3f50ec5ddd0dde50bf7246713c5575b297b93ba9
JuanJoseUTP0146906 <90533222+JuanJoseUTP0146906@users.noreply.github.com>
2024-11-23 10:27:13 -0600

    changes 3

--- lib/Screens/login/components/custom_sign_in.dart
+++ lib/Screens/login/components/custom_sign_in.dart
@@ -5,29 +5,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// Métodos de autenticación fuera de la interfaz
-Future<UserCredential> signInWithGoogle() async {
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-}
-
-Future<UserCredential> signInWithFacebook() async {
-  final LoginResult loginResult = await FacebookAuth.instance.login();
-
-  final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(
-    loginResult.accessToken!.token,
-  );
-
-  return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-}
-
 // Diálogo personalizado de inicio de sesión
 Future<Object?> customSigninDialog(BuildContext context, {required ValueChanged onClosed}) {
   return showGeneralDialog(
